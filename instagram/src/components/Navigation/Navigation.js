cf6c99eade360a29ccec2fcda8700eacefd67933
Himadri Nayak <himadri.nayak1@gmail.com>
2021-08-14 01:11:10 +0530

    initial commit

---
+++ instagram/src/components/Navigation/Navigation.js
@@ -0,0 +1,21 @@
+import "./navigation.css"
+import logo from "./InstagramLogo.png"
+import React from 'react'
+import { IconButton  } from "@chakra-ui/react"
+import {SearchIcon} from "@chakra-ui/icons"
+
+function Navigation() {
+    return (
+        <div className="navigation">
+            <div className="container">
+                <img className="logo" src ={logo} alt="instagram logo"/>
+                <div className="search">
+                    <span className="searchText">Search</span>
+                    <IconButton aria-label="Search database" icon={<SearchIcon />} />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Navigation;
